fe65906c360eb49e59c270627d12c1cfd35268eb
hjuuK <khj020508@naver.com>
2023-08-09 13:49:25 +0900

    수업 소스

--- src/main/java/models/works/SaveService.java
+++ src/main/java/models/works/SaveService.java
@@ -3,7 +3,7 @@ package models.works;
 public class SaveService {
     private WorkDao workDao;
 
-    public SaveService(WorkDao workDao) {
+    public SaveService(WorkDao workDao) { // 직접 생성하는 것이 아닌 외부에서 통제 가능하도록 (외부에서 주입)
         this.workDao = workDao;
     }
 
